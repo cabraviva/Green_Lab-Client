0ae73cc0f6d70f75b9d05c3545d1a54bed4c0071
greencoder001 <64889695+greencoder001@users.noreply.github.com>
2021-03-18 10:19:18 +0100

    C# Installer

---
+++ installer/Green_Lab Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Green_Lab_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean _DEBUG = true;
+
+            Console.WriteLine("Launching Green_Lab Client");
+
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.Arguments = "greencoder001/Green_Lab-Client-MC _green_lab-client-mc --logging win";
+            start.FileName = "gli.exe";
+            if (_DEBUG)
+            {
+                start.WindowStyle = ProcessWindowStyle.Normal;
+                start.CreateNoWindow = false;
+            } else
+            {
+                start.WindowStyle = ProcessWindowStyle.Hidden;
+                start.CreateNoWindow = true;
+            }
+            
+            int exitCode;
+
+            using (Process proc = Process.Start(start))
+            {
+                proc.WaitForExit();
+
+                // Retrieve the app's exit code
+                exitCode = proc.ExitCode;
+
+                Console.WriteLine("Exit Code: " + exitCode.ToString());
+            }
+        }
+    }
+}
