3da69f6dd6bab1e176ee0f99d743e0f0dad00882
greencoder001 <64889695+greencoder001@users.noreply.github.com>
2021-04-06 12:07:29 +0200

    Bugfix for #2 (fixes #2)

--- gui/app/lib/optifine-url.js
+++ gui/app/lib/optifine-url.js
@@ -1,16 +1,20 @@
-/* global fetch */
+/* global fetch, $ */
 
 async function getOptifineDownloadURL () {
-  const _response = (await (await fetch('https://optifine.net/downloads')).text())
-  const dlcontainer = document.createElement('div')
-  dlcontainer.innerHTML = _response.replace(/<img(.*?)>/g, '')
-  const a = dlcontainer.querySelector('.downloadTable.mainTable tr .colDownload a')
-  const splittedA = a.href.split('&url=')[1]
-  const response = (await (await fetch(splittedA)).text())
-  const c = document.createElement('div')
-  c.innerHTML = response.replace(/<img(.*?)>/g, '')
-  const dlurl = `https://optifine.net/downloadx${c.querySelector('.tableDownload tr .downloadButton a').href.split('/downloadx')[1]}`
-  return dlurl
+  try {
+    const _response = (await (await fetch('https://optifine.net/downloads')).text())
+    const dlcontainer = document.createElement('div')
+    dlcontainer.innerHTML = _response.replace(/<img(.*?)>/g, '')
+    const a = dlcontainer.querySelector('.downloadTable.mainTable tr .colDownload a')
+    const splittedA = a.href.split('&url=')[1]
+    const response = (await (await fetch(splittedA)).text())
+    const c = document.createElement('div')
+    c.innerHTML = response.replace(/<img(.*?)>/g, '')
+    const dlurl = `https://optifine.net/downloadx${c.querySelector('.tableDownload tr .downloadButton a').href.split('/downloadx')[1]}`
+    return dlurl
+  } catch {
+    $.page.refresh()
+  }
 }
 
 module.exports = getOptifineDownloadURL
